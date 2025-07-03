3dc6f75a1e84258936e0f258bc8ea21ad1983a4f
danielward27 <danielward27@outlook.com>
2025-07-03 10:35:03 +0100

    try adding pypi publishing

--- pymgcv/__init__.py
+++ pymgcv/__init__.py
@@ -1,14 +1,14 @@
 """pymgcv: Generalized Additive Models in Python."""
 
+from importlib.metadata import version
+
 from .gam import GAM
 
 __all__ = [
     "GAM",
-    "Interaction",
-    "Offset",
 ]
 
 # Version information
-__version__ = "0.0.0"
+__version__ = version("pymgcv")
 __author__ = "Daniel Ward"
 __email__ = "danielward27@outlook.com"
